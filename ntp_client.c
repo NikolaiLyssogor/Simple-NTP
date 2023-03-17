91336808ee6043c04e6149f8de37912787447076
NikolaiLyssogor <nily9199@colorado.edu>
2023-03-17 16:49:44 -0600

    computed local/server delta

--- ntp_client.c
+++ ntp_client.c
@@ -5,10 +5,13 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <sys/time.h>
 
 #define SERVERADDR "utcnist2.colorado.edu"
 #define SERVERPORT "123"
-#define PACKETSIZE = 48 // Size of our NTP packets
+#define PACKETSIZE 48 // size of our NTP packets
+#define NTP_EPOCH_OFFSET 2208988800UL // seconds between 1/1/1900 (NTP epoch) and 1/1/1970 (Unix epoch)
 
 typedef struct {
 
@@ -29,11 +32,11 @@ typedef struct {
     uint32_t org_ts_int;
     uint32_t org_ts_frac;
 
-    uint32_t rec_ts_int;
-    uint32_t rec_ts_frac;
+    uint32_t recv_ts_int;
+    uint32_t recv_ts_frac;
 
     uint32_t trans_ts_int;
-    uint32_t trains_ts_frac;
+    uint32_t trans_ts_frac;
     
 } ntp_packet;
 
@@ -80,7 +83,7 @@ int main() {
     // Prepare the NTP packet and send it
     ntp_packet packet;
     memset(&packet, 0, sizeof(packet)); // zero out the packet
-    packet.li_vn_mode = 0x23; //
+    packet.li_vn_mode = 0x23; // li = 0, vn = 4, mode = 3
     int bytes_sent = send(sockfd, &packet, sizeof(packet), 0);
     freeaddrinfo(servinfo);
 
@@ -92,9 +95,35 @@ int main() {
         exit(1);
     }
 
-    printf("bytest received: %d\n", bytes_recvd);
+    printf("bytest received: %d\n\n", bytes_recvd);
 
+    // Convert from network to host byte order
+    packet.stratum = ntohl(packet.stratum);
+    packet.poll = ntohl(packet.poll);
+    packet.recv_ts_int = ntohl(packet.recv_ts_int);
+    packet.recv_ts_frac = ntohl(packet.recv_ts_frac);
+    packet.trans_ts_int = ntohl(packet.trans_ts_int);
+    packet.trans_ts_frac = ntohl(packet.trans_ts_frac);
 
+    printf("stratum: %u\n", packet.stratum);
+    printf("poll: %u\n\n", packet.poll);
+    printf("receive seconds: %u\n", packet.recv_ts_int);
+    printf("receive fraction: %u\n\n", packet.recv_ts_frac);
+    printf("transmit seconds: %u\n", packet.trans_ts_int);
+    printf("transmit fraction: %u\n\n", packet.trans_ts_frac);
+
+    // Get local system time (Unix)
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    // Convert local Unix time to local NTP time
+    uint32_t local_ntp_int = tv.tv_sec + NTP_EPOCH_OFFSET;
+    printf("local seconds as NTP: %u\n", local_ntp_int);
+    uint32_t local_ntp_frac =  (uint32_t)((double)(tv.tv_usec + 1) * (double)(1LL << 32) * 1.0e-6); // https://tickelton.gitlab.io/articles/ntp-timestamps/
+    printf("local ms as NTP: %u\n", local_ntp_frac);
+
+    printf("server/local seconds diff: %u\n", packet.recv_ts_int - local_ntp_int);
+    printf("server/local ms diff: %u\n", packet.recv_ts_frac - local_ntp_frac);
 
     return 0;
 }
